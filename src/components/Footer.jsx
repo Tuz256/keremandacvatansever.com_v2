f2dd0c35db6ced2678cd6375370771a914de5278
Andac <keremandac240@gmail.com>
2026-08-22 16:31:36 +0300

    migrate "Footer.jsx" to "Footer.astro"

--- src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-import styles from '../styles/style';
-
-export default function Footer() {
-  return (
-    <footer style={styles.footer}>
-      © {new Date().getFullYear()} Kerem Andaç Vatansever · Tüm hakları saklıdır.
-    </footer>
-  );
-}
