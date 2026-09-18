aec58b18c36bd9f265c344ba5c5194bd4dad9b0f
Andac <keremandac240@gmail.com>
2026-09-18 11:24:51 +0300

    refactor(tags): add tags for pasta

--- src/data/tags.js
+++ src/data/tags.js
@@ -1,9 +1,14 @@
 export const TAGS = {
+
   meyveli: "meyveli",
   tatli: "tatlı",
   kitir: "kıtır-kıtır",
   nisastali: "nişastalı",
+  kiymali: "kıymalı",
   siviyag: "sıvı-yağ",
   katiyag: "katı-yağ",
+
+  azBulasik: "az-bulaşık",
+  kisaSurede: "kısa-sürede",
   elbecerisi: "el-becerisi",
 };
